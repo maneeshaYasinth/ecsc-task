f85682e362af843a9bbcd62ec88ebc4a6f26d01a
maneeshaYasinth <maneeshayasinthgunarathna@gmail.com>
2024-04-26 21:09:33 +0530

    added about us page

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-function showSidebar(){
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display = 'flex'
-  }
-  function hideSidebar(){
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display = 'none'
-  }
+function showSidebar() {
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.display = 'flex';
+  sidebar.classList.add('show-sidebar'); // Add class for transition effect
+}
+
+function hideSidebar() {
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.classList.remove('show-sidebar'); // Remove class for transition effect
+  setTimeout(() => { // Hide sidebar after transition completes
+    sidebar.style.display = 'none';
+  }, 300); // Adjust transition duration if needed
+}
