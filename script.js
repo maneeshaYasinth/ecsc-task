7f236bf9e045d97f7d5a2f9bd7368dd3af0368ca
maneeshaYasinth <maneeshayasinthgunarathna@gmail.com>
2024-05-01 23:18:58 +0530

    added more about us page

--- script.js
+++ script.js
@@ -11,3 +11,14 @@ function hideSidebar() {
     sidebar.style.display = 'none';
   }, 300); // Adjust transition duration if needed
 }
+
+
+document.getElementById('btn-more').addEventListener('click', function() {
+  document.getElementById('page-content').style.display = 'block'; // Display the page content when the button is clicked
+  document.getElementById('btn-more').style.display = 'none';
+});
+
+document.getElementById('btn-close').addEventListener('click', function() {
+  document.getElementById('page-content').style.display = 'none'; // Hide the page content when the close button is clicked
+  document.getElementById('btn-more').style.display = 'flex';
+});
