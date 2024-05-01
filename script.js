d763229b6c7c9e1626ca7089c3f1410df296a616
maneeshaYasinth <maneeshayasinthgunarathna@gmail.com>
2024-05-02 02:11:32 +0530

    updated more info

--- script.js
+++ script.js
@@ -12,13 +12,12 @@ function hideSidebar() {
   }, 300); // Adjust transition duration if needed
 }
 
-
 document.getElementById('btn-more').addEventListener('click', function() {
   document.getElementById('page-content').style.display = 'block'; // Display the page content when the button is clicked
-  document.getElementById('btn-more').style.display = 'none';
+  document.getElementById('btn-more').style.display = 'none'; // Hide the "more" button
 });
 
 document.getElementById('btn-close').addEventListener('click', function() {
   document.getElementById('page-content').style.display = 'none'; // Hide the page content when the close button is clicked
-  document.getElementById('btn-more').style.display = 'flex';
+  document.getElementById('btn-more').style.display = 'flex'; // Show the "more" button
 });
