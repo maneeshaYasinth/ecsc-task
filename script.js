f3b361aac833e3152a1f00508a78d331303fcab7
maneeshaYasinth <maneeshayasinthgunarathna@gmail.com>
2024-05-04 13:15:19 +0530

    finalized it

--- script.js
+++ script.js
@@ -22,3 +22,57 @@ document.getElementById('btn-close').addEventListener('click', function() {
   document.getElementById('btn-more').style.display = 'flex'; // Show the "more" button
 });
 
+$(document).ready(function(){
+  $('.slider').slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000, 
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+          arrows: false
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          dots: false,
+          arrows: false
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false,
+          arrows: false
+        }
+      }
+    ]
+  });
+
+  $('.slider').on('afterChange', function(event, slick, currentSlide){
+    $('.card-slider').removeClass('focused');
+    $('.slider .slick-active').eq(1).addClass('focused');
+  });
+
+  $('.card').on('keydown', function(event) {
+    if (event.key === 'Enter') {
+      $(this).focus();
+    }
+  });
+});
+
